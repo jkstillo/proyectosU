1544f60a746cd48182bc3534ee2694d16df2f8e3
JOSE CASTILLO <josecastillo@MacBook-Air-de-JOSE.local>
2021-02-21 16:01:48 -0600

    added Calculadora

--- Calculadora/src/main/java/com/mycompany/calculadora/Principal.java
+++ Calculadora/src/main/java/com/mycompany/calculadora/Principal.java
@@ -12,7 +12,8 @@ import java.util.Scanner;
  * @author josecastillo
  */
 public class Principal {
-        //Metodo principal
+    //Metodo principal
+
     public static void main(String[] args) {
 
         //Define variables
@@ -23,6 +24,8 @@ public class Principal {
             System.out.println("Seleccionar una opcion");
             System.out.println("A - Si desea sumar");
             System.out.println("B - Si desea restar");
+            System.out.println("C - Si desea multiplicar");
+            System.out.println("D - Si desea dicidir");
             System.out.println("Escriba su opcion:");
 
             // Recibe el valor del buffer del teclado
@@ -74,27 +77,64 @@ public class Principal {
                     // Sale del case
                     break;
 
+                case "C":
+                    strOpcion = "Multiplicar";
+                    System.out.println("");
+
+                    //Solicitar los valores numericos
+                    for (int i = 0; i < 2; i++) {
+                        System.out.println("Ingrese el numero " + (i + 1) + " : ");
+                        fltArrNumeros[i] = objInput.nextFloat();
+
+                    }
+
+                    fltResultado = fltArrNumeros[0] * fltArrNumeros[1];
+
+                    //Control
+                    blnError = false;
+
+                    // Sale del case
+                    break;
+
+                case "D":
+                    strOpcion = "Dividir";
+                    System.out.println("");
+
+                    //Solicitar los valores numericos
+                    for (int i = 0; i < 2; i++) {
+                        System.out.println("Ingrese el numero " + (i + 1) + " : ");
+                        fltArrNumeros[i] = objInput.nextFloat();
+
+                    }
+
+                    fltResultado = fltArrNumeros[0] / fltArrNumeros[1];
+
+                    //Control
+                    blnError = false;
+
+                    // Sale del case
+                    break;
+
                 default:
-	        //Error
+                    //Error
                     blnError = true;
 
                     // Sale del case
                     break;
-}
+            }
 
-System.out.println("");
+            System.out.println("");
 
-	         // Despliegue de los datos, si no hay error
-                    if (blnError == false) {
-                        System.out.println("El resultado es: " + fltResultado);
-                    } else if (blnError == true) {
-                        System.out.println("Error no se puede realizar su operacion");
-                    }
-                    System.out.println("La opcion seleccionada es " + strOpcion);
-                    
-                    System.out.println("Desea continuar? S/N");
-                    
-            
+            // Despliegue de los datos, si no hay error
+            if (blnError == false) {
+                System.out.println("El resultado es: " + fltResultado);
+            } else if (blnError == true) {
+                System.out.println("Error no se puede realizar su operacion");
+            }
+            System.out.println("La opcion seleccionada es " + strOpcion);
+
+            System.out.println("Desea continuar? S/N");
+
             // Capturar el buffer para continuar
             Scanner obInput2 = new Scanner(System.in);
             strContinuar = obInput2.nextLine();
@@ -102,5 +142,3 @@ System.out.println("");
 
     }
 }
-    
-
